02d3380921a3ee502c54ad250e007a431a70a72d
aaronli39 <aaronli39@gmail.com>
2019-01-30 22:33:49 -0500

    did hw

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,38 +1,54 @@
 var c = document.getElementById("slate");
 var clear = document.getElementById("clear");
 var tog = document.getElementById("toggle");
-var mode = "rectangle";
+var mode = "(boring) box";
 var cur = document.getElementById("cur");
 
 // instantiate a CanvasRenderingContext2D
 var ctx = c.getContext("2d");
-// invoke interface methods
-// ctx.fillStyle = "#ff0000";
-// ctx.strokeStyle = "#ff0000";
-// strokeStyle, clearRect(), fillText ellipse()
-// ctx.fillRect(80, 100, 100, 200);
-cur.innerHTML = "Current mode: " + mode;
 
-c.addEventListener("click", function(e) {
-	ctx.fillStyle = "#0000ff";
-	ctx.fillRect(e.clientX, e.clientY, 100, 200);
-	console.log(e.clientX);
-	console.log(e.clientY);
+// setting the current mode
+cur.innerHTML = "Current mode: " + mode;
+tog.addEventListener("click", function(e) {
+	if (mode == "(boring) box") {
+		mode = "(big) dot";
+		cur.innerHTML = "Current mode: " + mode;
+	} else {
+		mode = "(boring) box";
+		cur.innerHTML = "Current mode: " + mode;
+	}
 });
 
+// clearing canvas
 clear.addEventListener("click", function(e) {
 	ctx.clearRect(0, 0, c.height, c.width);
 });
 
-tog.addEventListener("click", function(e) {
-	if (mode == "rectangle") {
-		mode = "circle";
-		cur.innerHTML = "Current mode: " + mode;
-	} else {
-		mode = "rectangle";
-		cur.innerHTML = "Current mode: " + mode;
+// only draw if mouse within the canvas
+c.addEventListener("click", draw);
+// drawing whatever mode is on
+var draw = function(e) {
+	console.log("xcor: " + e.offsetX);
+	console.log("ycor: " + e.offsetY);
+	if (mode == "(boring) box") { // mode rect
+		// randomize color of box
+		ctx.fillStyle = "rgb(" + Math.floor(Math.random() * 256) + ", " +
+						Math.floor(Math.random() * 256) + ", " +
+						Math.floor(Math.random() * 256) + ")";
+		ctx.fillRect(e.offsetX - 50, e.offsetY - 75, 100, 150);
+	} else if (mode == "(big) dot") { // mode circle
+		ctx.beginPath();
+		ctx.strokeStyle = "rgb(" + Math.floor(Math.random() * 256) + ", " +
+						Math.floor(Math.random() * 256) + ", " +
+						Math.floor(Math.random() * 256) + ")";
+		ctx.fillStyle = "rgb(" + Math.floor(Math.random() * 256) + ", " +
+						Math.floor(Math.random() * 256) + ", " +
+						Math.floor(Math.random() * 256) + ")";
+		ctx.ellipse(e.offsetX, e.offsetY, 10, 15, 0, Math.PI, Math.PI * 3);
+		ctx.fill();
+		ctx.stroke();
 	}
-});
+};
 
 console.log(c);
 console.log(ctx);
