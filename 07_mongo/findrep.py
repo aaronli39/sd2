88a9e2f69bce0755047d01ea8fbb2ec373dde5aa
aaronli39 <aaronli39@gmail.com>
2019-03-04 21:28:37 -0500

    finished formatting

--- 07_mongo/findrep.py
+++ 07_mongo/findrep.py
@@ -2,17 +2,25 @@
 
 import sys
 
+
 def convert(name):
     try:
         f = open(name, "rU")
         line = f.read()
         line = str(line)
         # print(line)
+        f.close()
     except:
         print("bad stuff happened")
         return
-    line.replace(", \"event\":", "}\n{\"event\":")
-    print(line)
+    ret = line.replace("{\"event\":", "\n{\"event\":")
+    # print(line.replace("{\"event\":", "\n{\"event\":"))
+    try:
+        f = open(name, "w")
+        f.write(ret)
+        f.close()
+    except:
+        print("error writing")
 
 def main():
     convert(sys.argv[1])
