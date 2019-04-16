309b68617f2abde6bc3914871f3b8b448afaf6a6
aaronli39 <aaronli39@gmail.com>
2019-04-15 22:35:38 -0400

    finished

--- 18_listcomp/stuff.py
+++ 18_listcomp/stuff.py
@@ -1,7 +1,7 @@
 # Xiaojie(Aaron) Li, Hasif Ahmed
 # Softdev2 pd8
-# K18 -- PPFTLCW
 # 2019-04-15
+# K18 -- ListComp
 
 # pythagorean triples
 def triples(n):
@@ -9,6 +9,8 @@ def triples(n):
 
 # quiccccsort
 def qsort(inp):
+    if inp == []: return []
+    return qsort([i for i in inp[1:] if i <= inp[0]]) + [inp[0]] + qsort([i for i in inp[1:] if i > inp[0]])
     
-
 print(triples(17))
+print(qsort([1, 5, 7, 1, 3, 9, 10, 6, 8, 5, 3]))
