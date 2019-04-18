e7cff8c83c7c7038f885fe679010a847d12e1922
aaronli39 <aaronli39@gmail.com>
2019-04-17 22:18:02 -0400

    finished hw

--- 20_anon-reduce/temp.py
+++ 20_anon-reduce/temp.py
@@ -1,14 +1,25 @@
+'''
+Pineappols -- Xiaojie(Aaron) Li, Hasif Ahmed
+SoftDev2 pd8
+K20 -- Reductio ad Absurdum
+2019-04-17
+'''
+
+import re, functools
+
 with open("text.txt", "rU") as txt:
-    inp = txt.read().split("\n")
-    inp = [i.split(" ") for i in inp]
-    
-inp = inp[:len(inp) - 8]
-print(inp)
+    inp = txt.read()
 
-def freq(word):
-    return len([x for i in inp for x in i if x == word])
-
+# this searches for most frequent word or phrase
 def freqWords(words):
-    return 
+    return len([i for i in range(0, len(inp) - len(words)) if inp[i: i + len(words)] == words])
 
-print(freq("the"))
+word = re.findall(r'\w+', inp)
+words = list(set(word))
+
+# this returns most frequent word in moby dick
+def mostFreq():
+    return functools.reduce(lambda a, b: a if a[1] > b[1] else b, [(i, word.count(i)) for i in words])
+
+print(freqWords("the"))
+print(mostFreq())
